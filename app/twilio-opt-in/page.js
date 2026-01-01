3813639e6c10e01b2a9ae501639fd1edcb945e43
jrc9862 <jrc9862@stern.nyu.edu>
2026-01-01 01:13:58 -0500

    uncommented

--- app/twilio-opt-in/page.js
+++ app/twilio-opt-in/page.js
@@ -15,10 +15,10 @@ export default function TwilioOptIn() {
     <Layout>
       <AnimatedPage transitionType={transitionType}>
         <p>
-          {This is a page specifially for whomever is reviewing my site for my twilio opt-in form. Stop rejecting it. I am trying to do a side project and twilio is being a pain in my behind.}
+          This is a page specifially for whomever is reviewing my site for my twilio opt-in form. Stop rejecting it. I am trying to do a side project and twilio is being a pain in my behind.
         </p>
         <p>
-          {I, James Collett, am explicitly opting in to recieve text messages from James Collett about anything he (I) wants.}
+          I, James Collett, am explicitly opting in to recieve text messages from James Collett about anything he (I) wants.
         </p>
       </AnimatedPage>
     </Layout>
